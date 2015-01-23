51a1dc614c77f0616bf4f731eb82705b2369f519
2065093 <2065983Y@student.gla.ac.uk>
2015-01-23 21:21:58 +0000

    chapter 4 - completed

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Rango says wut up daug?")
+	return HttpResponse(
+		"<HTML> Rango says wut up daug? <br/>\
+		<a href='/rango/about'>About</a></HTML>")
+
+def about(requtest):
+	return HttpResponse("""Rango says here's dem about page lul 
+		<br/> <a href="/rango"> Home </a>
+		""")
+
